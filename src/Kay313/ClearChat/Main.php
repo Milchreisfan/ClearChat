5dc0b6afb7a060cd4994545aade9046ffe78021c
Kay313 <61843698+Kay313@users.noreply.github.com>
2020-06-07 19:50:12 +0200

    Update Main.php

--- src/Kay313/ClearChat/Main.php
+++ src/Kay313/ClearChat/Main.php
@@ -1,1 +1,26 @@
+<?php
 
+namespace Kay313\ClearChat;
+
+use pocketmine\command\Command;
+use pocketmine\event\Listener;
+use pocketmine\Player;
+use pocketmine\Server;
+use pocketmine\command\CommandSender;
+use pocketmine\command\ConsoleCommandSender;
+use pocketmine\plugin\PluginBase;
+
+class Main extends PluginBase implements Listener{
+	
+    public function onEnable(){
+        $this->getServer()->getLogger()->info("ClearChat enabled");
+    } 
+    public function onDisable(){
+	      $this->getServer()->getLogger()->info("ClearChat disabled");
+    }
+
+    public function onCommand(CommandSender $sender, Command $command, string $label, array $args): bool {
+            if(strtolower($command->getName()) == "cc"){
+                if ($sender instanceof Player) {
+                    if (!isset($args[0])) {
+                        if (!$sender->hasPermission("cc.use")) {
