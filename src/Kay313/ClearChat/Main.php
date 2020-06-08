597a594310591eaa828ace6e41d15276f6dc1b80
Kay313 <61843698+Kay313@users.noreply.github.com>
2020-06-08 11:18:46 +0200

    Update Main.php

--- src/Kay313/ClearChat/Main.php
+++ src/Kay313/ClearChat/Main.php
@@ -10,155 +10,156 @@ use pocketmine\command\CommandSender;
 use pocketmine\command\ConsoleCommandSender;
 use pocketmine\plugin\PluginBase;
 
-class Main extends PluginBase implements Listener{
-	
-    public function onEnable(){
-        $this->getServer()->getLogger()->info("ClearChat enabled");
-    } 
-    public function onDisable(){
-	$this->getServer()->getLogger()->info("ClearChat disabled");
-    }
+class Main extends PluginBase{
 
-    public function onCommand(CommandSender $sender, Command $command, string $label, array $args): bool {
-            if(strtolower($command->getName()) == "cc"){
+	public function onEnable(){
+		$this->getLogger()->info("ClearChat enabled!");
+	}
+	public function onDisable(){
+		$this->getLogger()->info("ClearChat disabled!");
+	}
+
+        public function onCommand(CommandSender $sender, Command $command, string $label, array $args): bool {
+        switch (strtolower($command->getName())) {
+            case "cc":
                 if ($sender instanceof Player) {
                     if (!isset($args[0])) {
                         if (!$sender->hasPermission("cc.use")) {
-			return true;
-			} else {
-                $senderName = $sender->getName();
-		$this->getServer()->broadcastMessage("");
-		$this->getServer()->broadcastMessage("");
-		$this->getServer()->broadcastMessage("");
-		$this->getServer()->broadcastMessage("");
-		$this->getServer()->broadcastMessage("");
-		$this->getServer()->broadcastMessage("");
-	     	$this->getServer()->broadcastMessage("");
-		$this->getServer()->broadcastMessage("");
-		$this->getServer()->broadcastMessage("");
-		$this->getServer()->broadcastMessage("");
-		$this->getServer()->broadcastMessage("");
-		$this->getServer()->broadcastMessage("");
-		$this->getServer()->broadcastMessage("");
-		$this->getServer()->broadcastMessage("");
-	     	$this->getServer()->broadcastMessage("");
-		$this->getServer()->broadcastMessage("");
-		$this->getServer()->broadcastMessage("");
-		$this->getServer()->broadcastMessage("");
-		$this->getServer()->broadcastMessage("");
-		$this->getServer()->broadcastMessage("");
-		$this->getServer()->broadcastMessage("");
-		$this->getServer()->broadcastMessage("");
-	     	$this->getServer()->broadcastMessage("");
-		$this->getServer()->broadcastMessage("");
-		$this->getServer()->broadcastMessage("");
-		$this->getServer()->broadcastMessage("");
-		$this->getServer()->broadcastMessage("");
-		$this->getServer()->broadcastMessage("");
-		$this->getServer()->broadcastMessage("");
-		$this->getServer()->broadcastMessage("");
-	     	$this->getServer()->broadcastMessage("");
-		$this->getServer()->broadcastMessage("");
-		$this->getServer()->broadcastMessage("");
-		$this->getServer()->broadcastMessage("");
-		$this->getServer()->broadcastMessage("");
-		$this->getServer()->broadcastMessage("");
-		$this->getServer()->broadcastMessage("");
-		$this->getServer()->broadcastMessage("");
-	     	$this->getServer()->broadcastMessage("");
-		$this->getServer()->broadcastMessage("");
-		$this->getServer()->broadcastMessage("");
-		$this->getServer()->broadcastMessage("");
-		$this->getServer()->broadcastMessage("");
-		$this->getServer()->broadcastMessage("");
-		$this->getServer()->broadcastMessage("");
-		$this->getServer()->broadcastMessage("");
-	     	$this->getServer()->broadcastMessage("");
-		$this->getServer()->broadcastMessage("");
-		$this->getServer()->broadcastMessage("");
-		$this->getServer()->broadcastMessage("");
-		$this->getServer()->broadcastMessage("");
-		$this->getServer()->broadcastMessage("");
-		$this->getServer()->broadcastMessage("");
-		$this->getServer()->broadcastMessage("");
-	     	$this->getServer()->broadcastMessage("");
-		$this->getServer()->broadcastMessage("");
-		$this->getServer()->broadcastMessage("");
-		$this->getServer()->broadcastMessage("");
-		$this->getServer()->broadcastMessage("");
-		$this->getServer()->broadcastMessage("");
-		$this->getServer()->broadcastMessage("");
-		$this->getServer()->broadcastMessage("");
-	     	$this->getServer()->broadcastMessage("");
-		$this->getServer()->broadcastMessage("");
-		$this->getServer()->broadcastMessage("");
-		$this->getServer()->broadcastMessage("");
-		$this->getServer()->broadcastMessage("");
-		$this->getServer()->broadcastMessage("");
-		$this->getServer()->broadcastMessage("");
-		$this->getServer()->broadcastMessage("");
-	     	$this->getServer()->broadcastMessage("");
-		$this->getServer()->broadcastMessage("");
-		$this->getServer()->broadcastMessage("");
-		$this->getServer()->broadcastMessage("");
-		$this->getServer()->broadcastMessage("");
-		$this->getServer()->broadcastMessage("");
-		$this->getServer()->broadcastMessage("");
-		$this->getServer()->broadcastMessage("");
-	     	$this->getServer()->broadcastMessage("");
-		$this->getServer()->broadcastMessage("");
-		$this->getServer()->broadcastMessage("");
-		$this->getServer()->broadcastMessage("");
-		$this->getServer()->broadcastMessage("");
-		$this->getServer()->broadcastMessage("");
-		$this->getServer()->broadcastMessage("");
-		$this->getServer()->broadcastMessage("");
-	     	$this->getServer()->broadcastMessage("");
-		$this->getServer()->broadcastMessage("");
-		$this->getServer()->broadcastMessage("");
-		$this->getServer()->broadcastMessage("");
-		$this->getServer()->broadcastMessage("");
-		$this->getServer()->broadcastMessage("");
-		$this->getServer()->broadcastMessage("");
-		$this->getServer()->broadcastMessage("");
-	     	$this->getServer()->broadcastMessage("");
-		$this->getServer()->broadcastMessage("");
-		$this->getServer()->broadcastMessage("");
-		$this->getServer()->broadcastMessage("");
-		$this->getServer()->broadcastMessage("");
-		$this->getServer()->broadcastMessage("");
-		$this->getServer()->broadcastMessage("");
-		$this->getServer()->broadcastMessage("");
-	     	$this->getServer()->broadcastMessage("");
-		$this->getServer()->broadcastMessage("");
-		$this->getServer()->broadcastMessage("");
-		$this->getServer()->broadcastMessage("");
-		$this->getServer()->broadcastMessage("");
-		$this->getServer()->broadcastMessage("");
-		$this->getServer()->broadcastMessage("");
-		$this->getServer()->broadcastMessage("");
-	     	$this->getServer()->broadcastMessage("");
-		$this->getServer()->broadcastMessage("");
-		$this->getServer()->broadcastMessage("");
-		$this->getServer()->broadcastMessage("");
-		$this->getServer()->broadcastMessage("");
-		$this->getServer()->broadcastMessage("");
-		$this->getServer()->broadcastMessage("");
-		$this->getServer()->broadcastMessage("");
-	     	$this->getServer()->broadcastMessage("");
-		$this->getServer()->broadcastMessage("");
-		$this->getServer()->broadcastMessage("");
-		$this->getServer()->broadcastMessage("");
-		$this->getServer()->broadcastMessage("");
-		$this->getServer()->broadcastMessage("");
-		$this->getServer()->broadcastMessage("");
-		$this->getServer()->broadcastMessage("");
-	     	$this->getServer()->broadcastMessage("");
-		$this->getServer()->broadcastMessage("");
-		$this->getServer()->broadcastMessage("§aThe Chat was cleart by §b$senderName");
-     }
+                            return true;
+                        } else {
+	$this->getServer()->broadcastMessage("§l§cThe Chat will now be deleted");
+	$this->getServer()->broadcastMessage("");
+	$this->getServer()->broadcastMessage("");
+	$this->getServer()->broadcastMessage("");
+	$this->getServer()->broadcastMessage("");
+	$this->getServer()->broadcastMessage("");
+	$this->getServer()->broadcastMessage("");
+	$this->getServer()->broadcastMessage("");
+	$this->getServer()->broadcastMessage("");
+	$this->getServer()->broadcastMessage("");
+	$this->getServer()->broadcastMessage("");
+	$this->getServer()->broadcastMessage("");
+	$this->getServer()->broadcastMessage("");
+	$this->getServer()->broadcastMessage("");
+	$this->getServer()->broadcastMessage("");
+	$this->getServer()->broadcastMessage("");
+	$this->getServer()->broadcastMessage("");
+	$this->getServer()->broadcastMessage("");
+	$this->getServer()->broadcastMessage("");
+	$this->getServer()->broadcastMessage("");
+	$this->getServer()->broadcastMessage("");
+	$this->getServer()->broadcastMessage("");
+	$this->getServer()->broadcastMessage("");
+	$this->getServer()->broadcastMessage("");
+	$this->getServer()->broadcastMessage("");
+	$this->getServer()->broadcastMessage("");
+	$this->getServer()->broadcastMessage("");
+	$this->getServer()->broadcastMessage("");
+	$this->getServer()->broadcastMessage("");
+	$this->getServer()->broadcastMessage("");
+	$this->getServer()->broadcastMessage("");
+	$this->getServer()->broadcastMessage("");
+	$this->getServer()->broadcastMessage("");
+	$this->getServer()->broadcastMessage("");
+	$this->getServer()->broadcastMessage("");
+	$this->getServer()->broadcastMessage("");
+	$this->getServer()->broadcastMessage("");
+	$this->getServer()->broadcastMessage("");
+	$this->getServer()->broadcastMessage("");
+	$this->getServer()->broadcastMessage("");
+	$this->getServer()->broadcastMessage("");
+	$this->getServer()->broadcastMessage("");
+	$this->getServer()->broadcastMessage("");
+	$this->getServer()->broadcastMessage("");
+	$this->getServer()->broadcastMessage("");
+	$this->getServer()->broadcastMessage("");
+	$this->getServer()->broadcastMessage("");
+	$this->getServer()->broadcastMessage("");
+	$this->getServer()->broadcastMessage("");
+	$this->getServer()->broadcastMessage("");
+	$this->getServer()->broadcastMessage("");
+	$this->getServer()->broadcastMessage("");
+	$this->getServer()->broadcastMessage("");
+	$this->getServer()->broadcastMessage("");
+	$this->getServer()->broadcastMessage("");
+	$this->getServer()->broadcastMessage("");
+	$this->getServer()->broadcastMessage("");
+	$this->getServer()->broadcastMessage("");
+	$this->getServer()->broadcastMessage("");
+	$this->getServer()->broadcastMessage("");
+	$this->getServer()->broadcastMessage("");
+	$this->getServer()->broadcastMessage("");
+	$this->getServer()->broadcastMessage("");
+	$this->getServer()->broadcastMessage("");
+	$this->getServer()->broadcastMessage("");
+	$this->getServer()->broadcastMessage("");
+	$this->getServer()->broadcastMessage("");
+	$this->getServer()->broadcastMessage("");
+	$this->getServer()->broadcastMessage("");
+	$this->getServer()->broadcastMessage("");
+	$this->getServer()->broadcastMessage("");
+	$this->getServer()->broadcastMessage("");
+	$this->getServer()->broadcastMessage("");
+	$this->getServer()->broadcastMessage("");
+	$this->getServer()->broadcastMessage("");
+	$this->getServer()->broadcastMessage("");
+	$this->getServer()->broadcastMessage("");
+	$this->getServer()->broadcastMessage("");
+	$this->getServer()->broadcastMessage("");
+	$this->getServer()->broadcastMessage("");
+	$this->getServer()->broadcastMessage("");
+	$this->getServer()->broadcastMessage("");
+	$this->getServer()->broadcastMessage("");
+	$this->getServer()->broadcastMessage("");
+	$this->getServer()->broadcastMessage("");
+	$this->getServer()->broadcastMessage("");
+	$this->getServer()->broadcastMessage("");
+	$this->getServer()->broadcastMessage("");
+	$this->getServer()->broadcastMessage("");
+	$this->getServer()->broadcastMessage("");
+	$this->getServer()->broadcastMessage("");
+	$this->getServer()->broadcastMessage("");
+	$this->getServer()->broadcastMessage("");
+	$this->getServer()->broadcastMessage("");
+	$this->getServer()->broadcastMessage("");
+	$this->getServer()->broadcastMessage("");
+	$this->getServer()->broadcastMessage("");
+	$this->getServer()->broadcastMessage("");
+	$this->getServer()->broadcastMessage("");
+	$this->getServer()->broadcastMessage("");
+	$this->getServer()->broadcastMessage("");
+	$this->getServer()->broadcastMessage("");
+	$this->getServer()->broadcastMessage("");
+	$this->getServer()->broadcastMessage("");
+	$this->getServer()->broadcastMessage("");
+	$this->getServer()->broadcastMessage("");
+	$this->getServer()->broadcastMessage("");
+	$this->getServer()->broadcastMessage("");
+	$this->getServer()->broadcastMessage("");
+	$this->getServer()->broadcastMessage("");
+	$this->getServer()->broadcastMessage("");
+	$this->getServer()->broadcastMessage("");
+	$this->getServer()->broadcastMessage("");
+	$this->getServer()->broadcastMessage("");
+	$this->getServer()->broadcastMessage("");
+	$this->getServer()->broadcastMessage("");
+	$this->getServer()->broadcastMessage("");
+	$this->getServer()->broadcastMessage("");
+	$this->getServer()->broadcastMessage("");
+	$this->getServer()->broadcastMessage("");
+  	$this->getServer()->broadcastMessage("");
+        $this->getServer()->broadcastMessage("");
+	$this->getServer()->broadcastMessage("");
+	$this->getServer()->broadcastMessage("");
+	$this->getServer()->broadcastMessage("");
+	$this->getServer()->broadcastMessage("");
+	$this->getServer()->broadcastMessage("");
+	$this->getServer()->broadcastMessage("§l§cThe Chat was deleted");
+       }
+      }
+     return false;
     }
    }
-  }
+  return true;
  }
 }
