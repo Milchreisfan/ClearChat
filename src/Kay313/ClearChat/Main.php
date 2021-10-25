c5a30f0e7b55a4996b12e862311c4ae5822e90e1
Jo <82047145+Milchreisfan@users.noreply.github.com>
2021-10-25 19:19:19 +0200

    bug fix lmao

--- src/Kay313/ClearChat/Main.php
+++ src/Kay313/ClearChat/Main.php
@@ -2,251 +2,53 @@
 
 namespace Kay313\ClearChat;
 
-use pocketmine\command\Command;
-use pocketmine\event\Listener;
-use pocketmine\Player;
-use pocketmine\Server;
-use pocketmine\command\CommandSender;
-use pocketmine\command\ConsoleCommandSender;
 use pocketmine\plugin\PluginBase;
+use pocketmine\event\Listener;
+use pocketmine\command\Command;
+use pocketmine\command\CommandSender;
+use pocketmine\command\SimpleCommandMap;
+use pocketmine\utils\TextFormat;
+use pocketmine\Server;
+use pocketmine\player\Player;
+use Datetime;
+use pocketmine\permission\DefaultPermissions;
+use pocketmine\permission\Permission;
 
-class Main extends PluginBase{
+# Command #
+use Kay313\ClearChat\commands\CC;
+use Kay313\ClearChat\commands\PCC;
 
-	public function onEnable(){
-		$this->getLogger()->info("ClearChat enabled!");
-	}
-	public function onDisable(){
-		$this->getLogger()->info("ClearChat disabled!");
-	}
-
-        public function onCommand(CommandSender $sender, Command $command, string $label, array $args): bool {
-        switch (strtolower($command->getName())) {
-            case "cc":
-                if ($sender instanceof Player) {
-                    if (!isset($args[0])) {
-                        if (!$sender->hasPermission("cc.use")) {
-                            return true;
-                        } else {
-	$this->getServer()->broadcastMessage("§l§cThe Chat will now be deleted");
-	$this->getServer()->broadcastMessage("§r");
-	$this->getServer()->broadcastMessage("§r");
-	$this->getServer()->broadcastMessage("§r");
-	$this->getServer()->broadcastMessage("§r");
-	$this->getServer()->broadcastMessage("§r");
-	$this->getServer()->broadcastMessage("§r");
-	$this->getServer()->broadcastMessage("§r");
-	$this->getServer()->broadcastMessage("§r");
-	$this->getServer()->broadcastMessage("§r");
-	$this->getServer()->broadcastMessage("§r");
-	$this->getServer()->broadcastMessage("§r");
-	$this->getServer()->broadcastMessage("§r");
-	$this->getServer()->broadcastMessage("§r");
-	$this->getServer()->broadcastMessage("§r");
-	$this->getServer()->broadcastMessage("§r");
-	$this->getServer()->broadcastMessage("§r");
-	$this->getServer()->broadcastMessage("§r");
-	$this->getServer()->broadcastMessage("§r");
-	$this->getServer()->broadcastMessage("§r");
-	$this->getServer()->broadcastMessage("§r");
-	$this->getServer()->broadcastMessage("§r");
-	$this->getServer()->broadcastMessage("§r");
-	$this->getServer()->broadcastMessage("§r");
-	$this->getServer()->broadcastMessage("§r");
-	$this->getServer()->broadcastMessage("§r");
-	$this->getServer()->broadcastMessage("§r");
-	$this->getServer()->broadcastMessage("§r");
-	$this->getServer()->broadcastMessage("§r");
-	$this->getServer()->broadcastMessage("§r");
-	$this->getServer()->broadcastMessage("§r");
-	$this->getServer()->broadcastMessage("§r");
-	$this->getServer()->broadcastMessage("§r");
-	$this->getServer()->broadcastMessage("§r");
-	$this->getServer()->broadcastMessage("§r");
-	$this->getServer()->broadcastMessage("§r");
-	$this->getServer()->broadcastMessage("§r");
-	$this->getServer()->broadcastMessage("§r");
-	$this->getServer()->broadcastMessage("§r");
-	$this->getServer()->broadcastMessage("§r");
-	$this->getServer()->broadcastMessage("§r");
-	$this->getServer()->broadcastMessage("§r");
-	$this->getServer()->broadcastMessage("§r");
-	$this->getServer()->broadcastMessage("§r");
-	$this->getServer()->broadcastMessage("§r");
-	$this->getServer()->broadcastMessage("§r");
-	$this->getServer()->broadcastMessage("§r");
-	$this->getServer()->broadcastMessage("§r");
-	$this->getServer()->broadcastMessage("§r");
-	$this->getServer()->broadcastMessage("§r");
-	$this->getServer()->broadcastMessage("§r");
-	$this->getServer()->broadcastMessage("§r");
-	$this->getServer()->broadcastMessage("§r");
-	$this->getServer()->broadcastMessage("§r");
-	$this->getServer()->broadcastMessage("§r");
-	$this->getServer()->broadcastMessage("§r");
-	$this->getServer()->broadcastMessage("§r");
-	$this->getServer()->broadcastMessage("§r");
-	$this->getServer()->broadcastMessage("§r");
-	$this->getServer()->broadcastMessage("§r");
-	$this->getServer()->broadcastMessage("§r");
-	$this->getServer()->broadcastMessage("§r");
-	$this->getServer()->broadcastMessage("§r");
-	$this->getServer()->broadcastMessage("§r");
-	$this->getServer()->broadcastMessage("§r");
-	$this->getServer()->broadcastMessage("§r");
-	$this->getServer()->broadcastMessage("§r");
-	$this->getServer()->broadcastMessage("§r");
-	$this->getServer()->broadcastMessage("§r");
-	$this->getServer()->broadcastMessage("§r");
-	$this->getServer()->broadcastMessage("§r");
-	$this->getServer()->broadcastMessage("§r");
-	$this->getServer()->broadcastMessage("§r");
-	$this->getServer()->broadcastMessage("§r");
-	$this->getServer()->broadcastMessage("§r");
-	$this->getServer()->broadcastMessage("§r");
-	$this->getServer()->broadcastMessage("§r");
-	$this->getServer()->broadcastMessage("§r");
-	$this->getServer()->broadcastMessage("§r");
-	$this->getServer()->broadcastMessage("§r");
-	$this->getServer()->broadcastMessage("§r");
-	$this->getServer()->broadcastMessage("§r");
-	$this->getServer()->broadcastMessage("§r");
-	$this->getServer()->broadcastMessage("§r");
-	$this->getServer()->broadcastMessage("§r");
-	$this->getServer()->broadcastMessage("§r");
-	$this->getServer()->broadcastMessage("§r");
-	$this->getServer()->broadcastMessage("§r");
-	$this->getServer()->broadcastMessage("§r");
-	$this->getServer()->broadcastMessage("§r");
-	$this->getServer()->broadcastMessage("§r");
-	$this->getServer()->broadcastMessage("§r");
-	$this->getServer()->broadcastMessage("§r");
-	$this->getServer()->broadcastMessage("§r");
-	$this->getServer()->broadcastMessage("§r");
-	$this->getServer()->broadcastMessage("§r");
-	$this->getServer()->broadcastMessage("§r");
-	$this->getServer()->broadcastMessage("§r");
-	$this->getServer()->broadcastMessage("§r");
-	$this->getServer()->broadcastMessage("§r");
-	$this->getServer()->broadcastMessage("§r");
-	$this->getServer()->broadcastMessage("§r");
-	$this->getServer()->broadcastMessage("§r");
-	$this->getServer()->broadcastMessage("§r");
-	$this->getServer()->broadcastMessage("§r");
-	$this->getServer()->broadcastMessage("§r");
-	$this->getServer()->broadcastMessage("§r");
-	$this->getServer()->broadcastMessage("§r");
-	$this->getServer()->broadcastMessage("§r");
-	$this->getServer()->broadcastMessage("§r");
-	$this->getServer()->broadcastMessage("§r");
-	$this->getServer()->broadcastMessage("§r");
-	$this->getServer()->broadcastMessage("§r");
-	$this->getServer()->broadcastMessage("§r");
-	$this->getServer()->broadcastMessage("§r");
-	$this->getServer()->broadcastMessage("§r");
-	$this->getServer()->broadcastMessage("§r");
-	$this->getServer()->broadcastMessage("§r");
-	$this->getServer()->broadcastMessage("§r");
-	$this->getServer()->broadcastMessage("§r");
-	$this->getServer()->broadcastMessage("§r");
-	$this->getServer()->broadcastMessage("§r");
-	$this->getServer()->broadcastMessage("§r");
-	$this->getServer()->broadcastMessage("§r");
-	$this->getServer()->broadcastMessage("§r");
-	$this->getServer()->broadcastMessage("§r");
-	$this->getServer()->broadcastMessage("§r");
-	$this->getServer()->broadcastMessage("§r");
-	$this->getServer()->broadcastMessage("§r");
-	$this->getServer()->broadcastMessage("§r");
-	$this->getServer()->broadcastMessage("§r");
-	$this->getServer()->broadcastMessage("§r");
-	$this->getServer()->broadcastMessage("§r");
-	$this->getServer()->broadcastMessage("§r");
-	$this->getServer()->broadcastMessage("§r");
-	$this->getServer()->broadcastMessage("§r");
-	$this->getServer()->broadcastMessage("§r");
-	$this->getServer()->broadcastMessage("§r");
-	$this->getServer()->broadcastMessage("§r");
-	$this->getServer()->broadcastMessage("§r");
-	$this->getServer()->broadcastMessage("§r");
-	$this->getServer()->broadcastMessage("§r");
-	$this->getServer()->broadcastMessage("§r");
-	$this->getServer()->broadcastMessage("§r");
-	$this->getServer()->broadcastMessage("§r");
-	$this->getServer()->broadcastMessage("§r");
-	$this->getServer()->broadcastMessage("§r");
-	$this->getServer()->broadcastMessage("§r");
-	$this->getServer()->broadcastMessage("§r");
-	$this->getServer()->broadcastMessage("§r");
-	$this->getServer()->broadcastMessage("§r");
-	$this->getServer()->broadcastMessage("§r");
-	$this->getServer()->broadcastMessage("§r");
-	$this->getServer()->broadcastMessage("§r");
-	$this->getServer()->broadcastMessage("§r");
-	$this->getServer()->broadcastMessage("§r");
-	$this->getServer()->broadcastMessage("§r");
-	$this->getServer()->broadcastMessage("§r");
-	$this->getServer()->broadcastMessage("§r");
-	$this->getServer()->broadcastMessage("§r");
-	$this->getServer()->broadcastMessage("§r");
-	$this->getServer()->broadcastMessage("§r");
-	$this->getServer()->broadcastMessage("§r");
-	$this->getServer()->broadcastMessage("§r");
-	$this->getServer()->broadcastMessage("§r");
-	$this->getServer()->broadcastMessage("§r");
-	$this->getServer()->broadcastMessage("§r");
-	$this->getServer()->broadcastMessage("§r");
-	$this->getServer()->broadcastMessage("§r");
-	$this->getServer()->broadcastMessage("§r");
-	$this->getServer()->broadcastMessage("§r");
-	$this->getServer()->broadcastMessage("§r");
-	$this->getServer()->broadcastMessage("§r");
-	$this->getServer()->broadcastMessage("§r");
-	$this->getServer()->broadcastMessage("§r");
-	$this->getServer()->broadcastMessage("§r");
-	$this->getServer()->broadcastMessage("§r");
-	$this->getServer()->broadcastMessage("§r");
-	$this->getServer()->broadcastMessage("§r");
-	$this->getServer()->broadcastMessage("§r");
-	$this->getServer()->broadcastMessage("§r");
-	$this->getServer()->broadcastMessage("§r");
-	$this->getServer()->broadcastMessage("§r");
-	$this->getServer()->broadcastMessage("§r");
-	$this->getServer()->broadcastMessage("§r");
-	$this->getServer()->broadcastMessage("§r");
-	$this->getServer()->broadcastMessage("§r");
-	$this->getServer()->broadcastMessage("§r");
-	$this->getServer()->broadcastMessage("§r");
-	$this->getServer()->broadcastMessage("§r");
-	$this->getServer()->broadcastMessage("§r");
-	$this->getServer()->broadcastMessage("§r");
-	$this->getServer()->broadcastMessage("§r");
-	$this->getServer()->broadcastMessage("§r");
-	$this->getServer()->broadcastMessage("§r");
-	$this->getServer()->broadcastMessage("§r");
-	$this->getServer()->broadcastMessage("§r");
-	$this->getServer()->broadcastMessage("§r");
-	$this->getServer()->broadcastMessage("§r");
-	$this->getServer()->broadcastMessage("§r");
-	$this->getServer()->broadcastMessage("§r");
-	$this->getServer()->broadcastMessage("§r");
-	$this->getServer()->broadcastMessage("§r");
-	$this->getServer()->broadcastMessage("§r");
-	$this->getServer()->broadcastMessage("§r");
-	$this->getServer()->broadcastMessage("§r");
-	$this->getServer()->broadcastMessage("§r");
-	$this->getServer()->broadcastMessage("§r");
-	$this->getServer()->broadcastMessage("§r");
-	$this->getServer()->broadcastMessage("§r");
-	$this->getServer()->broadcastMessage("§r");
-	$this->getServer()->broadcastMessage("§r");
-	$this->getServer()->broadcastMessage("§r");
-	$this->getServer()->broadcastMessage("§r");
-	$this->getServer()->broadcastMessage("§l§cThe Chat was deleted");
-       }
-      }
-     return true;
+class Main extends PluginBase implements Listener
+{
+    public function onLoad(): void
+    {
+        self::$instance = $this;
+        $this->getLogger()->warning("PM-4.0.0 IS NOT A STABLE RELEASE!! Please REPORT bugs at https://github.com/Kay313/ClearChat !!"); 
     }
-   }
-  return false;
- }
+
+    public function onEnable(): void
+    {
+        $this->getServer()->getCommandMap()->registerAll("clearchat", [
+            new CC(),
+            new PPC()
+        ]);
+        $this->getLogger()->info("ClearChat enabled!");
+    }
+
+    public function onDisable(): void
+    {
+        $this->getScheduler()->cancelAllTasks();
+        $this->getLogger()->info("ClearChat disabled!");
+    }
+
+    public static function getInstance(): void
+    {
+        return self::$instance
+    }
+
+    public function registerPermission(): void
+    {
+        DefaultPermissions::registerPermission(new Permission("cc.use")); # chat clear for all players
+        DefaultPermissions::registerPermission(new Permission("pcc.use")); # private chat clear
+    }
 }
